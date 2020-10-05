df19f0ea6cafe9cca193547a0f54bfc1ffa9b61d
daniel-luong-1080 <62417581+daniel-luong-1080@users.noreply.github.com>
2020-10-05 13:15:18 +0700

    Update code

--- src/FormBlock.java
+++ src/FormBlock.java
@@ -1,6 +1,18 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import javax.swing.*;
 
 
 public class FormBlock {
-
+	private int[][] Block;
+	private GridBoard Board;
+	private BufferedImage BlockImg;
+	
+	public void BlockUpdate() {
+		
+	}
+	
+	public void BLockRender(Graphics Render) {
+		
+	}
 }
--- src/GridBoard.java
+++ src/GridBoard.java
@@ -1,13 +1,41 @@
 import java.awt.Graphics;
+import java.awt.Shape;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 
 public class GridBoard extends JPanel {
+	private BufferedImage Block;
 	public int COLUMNS = 15, ROWS = 30, BLOCKSIZE = 20;
 	public int[][] GRID = new int[COLUMNS][ROWS];
+	private FormBlock[] Shape = new FormBlock[7];
+	
+	
+	public GridBoard() {
+		try {
+			Block = ImageIO.read(new FileInputStream("resources/0.png"));
+			Block = ImageIO.read(new FileInputStream("resources/1.png"));
+			Block = ImageIO.read(new FileInputStream("resources/2.png"));
+			Block = ImageIO.read(new FileInputStream("resources/3.png"));
+			Block = ImageIO.read(new FileInputStream("resources/4.png"));
+			Block = ImageIO.read(new FileInputStream("resources/5.png"));
+			Block = ImageIO.read(new FileInputStream("resources/6.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Error in printing blocks");
+		}
+		
+		//Shapes
+		//Shape[0] = new FormBlock(Block.get);
+	}
 	
 	public void paintComponent(Graphics Draw) {
 		super.paintComponent(Draw);
+		Draw.drawImage(Block,0,0,null);
 		for (int i=0; i<ROWS; i++) {
 			Draw.drawLine(0, i*BLOCKSIZE, COLUMNS*BLOCKSIZE, i*BLOCKSIZE);
 		}
