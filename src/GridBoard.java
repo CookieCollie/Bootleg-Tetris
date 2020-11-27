bcb1dc2bcdd67896e068508ff963c7e810048a70
daniel-luong-1080 <62417581+daniel-luong-1080@users.noreply.github.com>
2020-11-27 13:35:12 +0700

    Update GridBoard.java

--- src/GridBoard.java
+++ src/GridBoard.java
@@ -6,7 +6,6 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Random;
 
